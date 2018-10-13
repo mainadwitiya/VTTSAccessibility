465b3535f71eed6e29dddf4be754db2a8e98e7a9
Tushar Paliwal <tushar.paliwal1997@gmail.com>
2018-10-13 18:12:24 +0530

    Add functionality to get curson location

---
+++ project.py
@@ -0,0 +1,53 @@
+"""Get the current mouse position."""
+
+import logging
+import sys
+import time
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG,
+                    stream=sys.stdout)
+
+
+def get_mouse_position():
+    """
+    Get the current position of the mouse.
+
+    Returns
+    -------
+    dict :
+        With keys 'x' and 'y'
+    """
+    mouse_position = None
+    import sys
+    if sys.platform in ['linux', 'linux2']:
+        pass
+    elif sys.platform == 'win32':
+        try:
+            import win32api
+        except ImportError:
+            logging.info("win32api not installed")
+            win32api = None
+        if win32api is not None:
+            x, y = win32api.GetCursorPos()
+            mouse_position = {'x': x, 'y': y}
+    elif sys.platform == 'Mac':
+        pass
+    else:
+        try:
+            import Tkinter  # Tkinter could be supported by all systems
+        except ImportError:
+            logging.info("Tkinter not installed")
+            Tkinter = None
+        if Tkinter is not None:
+            p = Tkinter.Tk()
+            x, y = p.winfo_pointerxy()
+            mouse_position = {'x': x, 'y': y}
+        print("sys.platform={platform} is unknown. Please report."
+              .format(platform=sys.platform))
+        print(sys.version)
+    return mouse_position
+
+while(True):
+    print(get_mouse_position())
+    time.sleep(2)
